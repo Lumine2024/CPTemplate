8594e3af2bc5911875a72f67491ef9aa55114019
Lumine2024 <diaochan2021@outlook.com>
2025-07-14 22:14:04 +0800

    计算几何基础

--- Geometry/Andrew.hpp
+++ Geometry/Andrew.hpp
@@ -1,35 +1,32 @@
 #pragma once
 #include <bits/stdc++.h>
+#include "Geometry/Basic.hpp"
 using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 
-struct Point {
-	double x, y;
-};
-double cross(const Point &a, const Point &b, const Point &c) {
-	return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
-}
 vector<Point> andrew(vector<Point> &points) {
 	sort(points.begin(), points.end(), [](const Point &a, const Point &b) {
-		return a.x == b.x ? a.y < b.y : a.x < b.x;
+		return a.x < b.x || (a.x == b.x && a.y < b.y);
 	});
 	int n = points.size();
+	if(n <= 2) {
+		return points;
+	}
 	vector<Point> stk;
-	for(int i = 1; i < n; ++i) {
-		while(stk.size() > 1 &&
-			cross(stk[stk.size() - 2], stk.back(), points[i]) <= 0) {
+	for(int i = 0; i < n; ++i) {
+		while(stk.size() >= 2 && cross(stk[stk.size() - 2], stk.back(), points[i]) <= 0) {
 			stk.pop_back();
-			}
+		}
 		stk.push_back(points[i]);
 	}
 	int t = stk.size();
-	for(int i = n - 1; i >= 0; --i) {
-		while(stk.size() > t &&
-			cross(stk[stk.size() - 2], stk.back(), points[i]) <= 0) {
+	for(int i = n - 2; i >= 0; --i) {
+		while(stk.size() > t && cross(stk[stk.size() - 2], stk.back(), points[i]) <= 0) {
 			stk.pop_back();
-			}
+		}
 		stk.push_back(points[i]);
 	}
+	stk.pop_back();
 	return stk;
 }
