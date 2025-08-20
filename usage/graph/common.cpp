bac6a56e17573e89cbfa02a7eeda3a4eddf3f48f
Lumine2024 <diaochan2021@outlook.com>
2025-08-20 13:52:52 +0000

    Remove unnecessary constants from standalone C++ files based on module usage
    Co-authored-by: Lumine2024 <149254783+Lumine2024@users.noreply.github.com>

--- usage/graph/common.cpp
+++ usage/graph/common.cpp
@@ -8,17 +8,9 @@ using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
 
-inline constexpr ll modulo = 998244353, g = 3, inf = 0x3f3f3f3f3f3f3f3f;
+inline constexpr ll inf = 0x3f3f3f3f3f3f3f3f;
 inline constexpr int maxn = 100005, infint = 0x3f3f3f3f;
-inline constexpr ld eps = 1e-9l, pi = 3.14159265358979323846264338327950288l, infld = 1e12l;
-
-inline ll qpow(ll x, ll n) {
-    ll ret = 1;
-    for(; n != 0; n >>= 1, x = x * x % modulo) {
-        if(n & 1) ret = ret * x % modulo;
-    }
-    return ret;
-}
+inline constexpr ld eps = 1e-9l, infld = 1e12l;
 
 template<class T, class F> bool chkf(T &x, const T &y, F &&f) {
     if(f(y, x)) {
