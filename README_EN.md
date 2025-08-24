8c10a5f52ee4bc8e7f613978157ec6c6f077fc7f
Lumine2024 <diaochan2021@outlook.com>
2025-08-24 18:53:39 +0800

    pick定理

---
+++ README_EN.md
@@ -0,0 +1,21 @@
+# Competitive Programming Template
+I have trimmed my competitive programming templates into two versions: one is markdown document(and the exported PDF from it); another is trimmed .cpp files, which can be pasted to contest field to directly form your solution below.
+
+## Features
+1. Excapsulated, suitable for convenient usage. For most of the bottom implementations I used wrapper functions(like use `query(l, r)` to wrap `_query(l, r, 0, 0, n)` in segment tree) to avoid unneccessary errors
+2. Readability, following the project specifics. Balancing the need for readability and contest, my codes are convenient for learning and usage during contests.
+3. Using 0-indexed and left-closed, right-open intervals, avoiding classified discussion, and matches up with C++ STL containers
+4. Having neccessary comments when in need, avoiding errors when use
+
+## Usage
+Download the zipped file and extract them, print the markdown document for regionals, and paste the cpp files for online tests(like Codeforces)
+
+## Contributions
+This repository is an outcome for my personal learning, so contributions are temporary unaccepted. If you want to modify them, you have to fork this repository into your own account
+
+However, if you have something that need me to implement, you can make an Issue to me
+
+---
+
+Finally, wish you can all have a nice trip on ICPC/OI!
+
--- usage/geo/convex.cpp
+++ usage/geo/convex.cpp
@@ -268,59 +268,110 @@ pair<Point, Point> inter(const Circle &c, const Line &l) {
 	return {i1, i2};
 }
 
-
-vector<Line> half_inter(vector<Line> lines) {
-	lines.push_back({{-inf, 0.0l}, {0.0l, -1.0l}});
-	lines.push_back({{inf, 0.0l}, {0.0l, 1.0l}});
-	lines.push_back({{0.0l, inf}, {-1.0l, 0.0l}});
-	lines.push_back({{0.0l, -inf}, {1.0l, 0.0l}});
-	sort(lines.begin(), lines.end(), [](const Line &a, const Line &b) {
-		int c = cmp(a.v.arg(), b.v.arg());
-		if(c != 0)
-			return c == -1;
-		return sign(cross(a.v, b.p - a.p)) < 0;
-	});
-	deque<Line> dq;
-	auto bad = [](const Line &l, const Line &b, const Line &c) {
-		try {
-			auto p = inter(b, c);
-			return to_left(l, p) < 0;
-		} catch(const invalid_argument &) {
-			return false;
-		}
+struct Convex : public Polygon {
+	using Polygon::Polygon;
+	// todo
+};
+bool is_in(const Point &pt, const Convex &convex) {
+	if(convex.size() == 1) {
+		return pt == convex.pts[0];
+	}
+	if(convex.size() == 2) {
+		return is_on(pt, Lineseg(convex.pts[0], convex.pts[1]));
+	}
+	auto check = [](const Point &a, const Point &b, const Point &c, const Point &p) {
+		ld c1 = cross(b - a, p - a), c2 = cross(c - b, p - b), c3 = cross(a - c, p - c);
+		return (sign(c1) != -1 && sign(c2) != -1 && sign(c3) != -1) 
+			|| (sign(c1) != 1 && sign(c2) != 1 && sign(c3) != 1);
 	};
-	for(auto &l : lines) {
-		if(!dq.empty() && cmp(l.v.arg(), dq.back().v.arg()) == 0) {
-			if(to_left(l, dq.back().p + dq.back().v) < 0)
-				dq.back() = l;
-			continue;
-		}
-		while(dq.size() > 1 && bad(l, dq.back(), dq[dq.size() - 2]))
-			dq.pop_back();
-		while(dq.size() > 1 && bad(l, dq[0], dq[1]))
-			dq.pop_front();
-		dq.push_back(l);
+	int n = convex.size();
+	Point pivot = convex.pts[0];
+	if((sign(cross(convex.pts[1] - pivot, pt - pivot)) == -1) 
+	|| (sign(cross(convex.pts[n - 1] - pivot, pt - pivot)) == 1)) {
+		return false;
 	}
-	while(dq.size() > 1 && bad(dq[0], dq.back(), dq[dq.size() - 2]))
-		dq.pop_back();
-	while(dq.size() > 1 && bad(dq.back(), dq[0], dq[1]))
-		dq.pop_front();
-	vector<Line> ret(dq.begin(), dq.end());
-	int m = ret.size();
-	if(m < 3)
-		return {};
-	vector<Point> poly;
-	poly.reserve(m);
-	for(int i = 0; i < m; ++i) {
-		try {
-			poly.emplace_back(inter(ret[i], ret[(i + 1) % m]));
-		} catch(const invalid_argument &) {
-			return {};
+	int l = 1, r = n - 1;
+	while(l + 1 < r) {
+		int mid = (l + r) >> 1;
+		if(sign(cross(convex.pts[mid] - pivot, pt - pivot)) != -1) {
+			l = mid;
+		} else {
+			r = mid;
 		}
 	}
-	if(poly.size() != m)
-		return {};
-	return ret;
+	int nxt = (l == n - 1) ? 1 : l + 1;
+	return check(pivot, convex.pts[l], convex.pts[nxt], pt);
+}
+pair<Point, Point> tangent(const Point &pt, const Convex &convex) {
+	if(is_in(pt, convex)) throw runtime_error("pt is in the convex");
+	int n = convex.size();
+	auto peak = [&](int l, int r, bool find_r) {
+		while(l < r - 1) {
+			int mid = (l + r) / 2;
+			if(find_r ^ (to_left(convex.pts[(mid + n - 1) % n] - pt, convex.pts[mid] - pt) == 1)) {
+				r = mid;
+			} else {
+				l = mid;
+			}
+		}
+		return l;
+	};
+	if(to_left(convex.pts[0] - pt, convex.pts[1] - pt) == 0) {
+		int idx = peak(2, n, cmp(dist(convex.pts[0], pt), dist(convex.pts[1], pt)) == -1);
+		return { convex.pts[0], convex.pts[idx] };
+	}
+	bool all_left = true, all_right = true;
+	auto chk = [&](int x) {
+		if(x == 1) all_right = false;
+		if(x == -1) all_left = false;
+	};
+	chk(to_left(convex.pts[0] - pt, convex.pts[1] - pt));
+	chk(to_left(convex.pts[0] - pt, convex.pts[n - 1] - pt));
+	if(all_left || all_right) {
+		int idx = peak(1, n, all_left);
+		return { convex.pts[0], convex.pts[idx] };
+	}
+	int l = 1, r = n;
+	while(l < r - 1) {
+		int mid = (l + r) / 2;
+		if(to_left(convex.pts[0] - pt, convex.pts[mid] - pt) == 1) {
+			l = mid;
+		} else {
+			r = mid;
+		}
+	}
+	int split = l;
+	bool flag = (to_left(convex.pts[0] - pt, convex.pts[1] - pt) == 1);
+	int i1 = peak(0, split + 1, flag), i2 = peak(split, n, !flag);
+	return { convex.pts[i1], convex.pts[i2] };
+}
+pair<Point, Point> tangent(const Line &ln, const Convex &convex) {
+	int n = convex.size();
+	Vector dir(-ln.v.y, ln.v.x);
+	auto find = [&](bool flag) {
+		int l = 0, r = n - 1;
+		while(r - l > 2) {
+			int m1 = l + (r - l) / 3, m2 = r - (r - l) / 3;
+			ld dot1 = dot(convex.pts[m1], dir), dot2 = dot(convex.pts[m2], dir);
+			if(flag ^ (cmp(dot1, dot2) == -1)) {
+				r = m2;
+			} else {
+				l = m1;
+			}
+		}
+		int idx = l;
+		ld ret = dot(convex.pts[l], dir);
+		for(int i = l; i <= r; ++i) {
+			ld now = dot(convex.pts[i], dir);
+			if(flag ^ (cmp(now, ret) == -1)) {
+				idx = i;
+				ret = now;
+			}
+		}
+		return idx;
+	};
+	int i1 = find(true), i2 = find(false);
+	return { convex.pts[i1], convex.pts[i2] };
 }
 
 inline void solve() {
