3da1585d274dac0dad19b4ca546f474bd1d14a15
Lumine2024 <diaochan2021@outlook.com>
2025-11-20 00:32:52 +0800

    对拍器

--- usage/run/comparer.ps1
+++ usage/run/comparer.ps1
@@ -1,41 +1,27 @@
-function compile($file, $out) {
-    g++ $file -o $out -std=c++20 -O2 -Wall
-    if($LASTEXITCODE -ne 0) {
-        Write-Host "compile error $file" -ForegroundColor Red
-        exit 1
-    }
-}
-function check2($msg) {
-    if($LASTEXITCODE -ne 0) {
-        Write-Host "Runtime error: $msg, exit code is $LASTEXITCODE" -ForegroundColor Red
-        exit 2
-    }
-}
-compile "generator.cpp" "gen.exe"
-compile "brute_force.cpp" "bf.exe"
-compile "solution.cpp" "sol.exe"
-mkdir -p testcases -Force
+Set-Alias println Write-Host
+
+g++ gen.cpp -o gen -std=c++23 -O2
+g++ sol.cpp -o sol -std=c++23 -O2
+g++ sol2.cpp -o sol2 -std=c++23 -O2
+mkdir -p tc -Force
 $cnt = 1
 while($cnt -le 100000) {
-    Write-Host "Running test case $cnt"
-    ./gen.exe > "testcases/input.txt"
-    check2 "generator"
-    Get-Content "testcases/input.txt" | ./sol.exe > "testcases/sol.out"
-    check2 "solution"
-    Get-Content "testcases/input.txt" | ./bf.exe > "testcases/bf.out"
-    check2 "brute_force"
-    $f1 = (Get-Content "testcases/sol.out" -Raw) -replace '\s+', ''
-    $f2 = (Get-Content "testcases/bf.out" -Raw) -replace '\s+', ''
+    println "Running $cnt"
+    ./gen > "tc/input.txt"
+    cat "tc/input.txt" | ./sol > "tc/sol.txt"
+    cat "tc/input.txt" | ./sol2 > "tc/sol2.txt"
+    $f1 = (cat "tc/sol.txt" -Raw) -replace '\s+', ''
+    $f2 = (cat "tc/sol2.txt" -Raw) -replace '\s+', ''
     if($f1 -ne $f2) {
-        Write-Host "Wrong answer on $cnt" -ForegroundColor Red
-        Write-Host "Input:"
-        Get-Content "testcases/input.txt"
-        Write-Host "Solution output:"
-        Get-Content "testcases/sol.out"
-        Write-Host "Brute force output:"
-        Get-Content "testcases/bf.out"
-        exit 3
+        println "Wrong answer" -ForegroundColor Red
+        println "Input:"
+        cat "tc/input.txt"
+        println "Sol1:"
+        cat "tc/sol.txt"
+        println "Sol2:"
+        cat "tc/sol2.txt"
+        break
     }
-    Write-Host "Accepted" -ForegroundColor Green
+    println "Accepted" -ForegroundColor Green
     $cnt++
 }
--- usage/run/comparer.sh
+++ usage/run/comparer.sh
@@ -1,31 +1,23 @@
-g++ generator.cpp -o gen.out -std=c++20 -O2 || exit 1
-g++ solution.cpp -o sol.out -std=c++20 -O2 -Wall || exit 1
-g++ brute_force.cpp -o bf.out -std=c++20 -O2 || exit 1
+#!/bin/bash
+g++ gen.cpp -o gen -std=c++20 -O2 || exit 1
+g++ sol1.cpp -o sol1 -std=c++20 -O2 -Wall || exit 1
+g++ sol2.cpp -o sol2 -std=c++20 -O2 || exit 1
 mkdir -p tc
 cnt=1
-while true; do
+while [ $cnt -le 100000 ]; do
 	echo "Running test $cnt"
-	./gen.out > tc/input.txt
-	timeout 2s ./sol.out < tc/input.txt > tc/sol.txt
-	exit_code=$?
-	if [ $exit_code -ne 0 ]; then
-		echo -e "\033[31mRuntime error or time limit exceeded on solution\033[0m"
-		break
-	fi
-	timeout 2s ./bf.out < tc/input.txt > tc/bf.txt
-	if [ $exit_code -ne 0 ]; then
-		echo -e "\033[31mRuntime error or time limit exceeded on brute force\033[0m"
-		break
-	fi
-	if ! diff -wB tc/sol.txt tc/bf.txt > /dev/null; then
+	./gen > tc/input.txt
+	timeout 2s ./sol1 < tc/input.txt > tc/sol1.txt || exit 2
+	timeout 2s ./sol2 < tc/input.txt > tc/sol2.txt || exit 2
+	if ! diff -wB tc/sol1.txt tc/sol2.txt > /dev/null; then
 		echo -e "\033[31mWrong Answer on test case $cnt\033[0m"
 		echo "Input:"
 		cat tc/input.txt
-		echo -e "\nSolution output:"
-		cat tc/sol.txt
-		echo -e "\nBrute force output:"
-		cat tc/bf.txt
-		break
+		echo -e "\nSol1:"
+		cat tc/sol1.txt
+		echo -e "\nSol2:"
+		cat tc/sol2.txt
+		exit 1
 	fi
 	echo -e "\033[32mAccepted\033[0m"
 	((cnt++))
